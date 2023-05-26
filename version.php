960dee3e0689bcdf8db6de3b578eeaa7c2450d17
Matthias Kollenbroich <matthias.kollenbroich@uni-muenster.de>
2023-05-26 16:02:47 +0200

    Updated version.php

--- version.php
+++ version.php
@@ -25,11 +25,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_opencast';
-$plugin->release   = 'development-version';
-$plugin->version = 2023030100;
+$plugin->release = 'v4.1-r1';
+$plugin->version = 2023052600;
 $plugin->requires = 2020061500; // Requires Moodle 3.9+.
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->supported = [39, 410];
+$plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'tool_opencast' => 2023030100,
-    'mod_opencast' => 2023030100
+    'tool_opencast' => 2023052600,
+    'mod_opencast' => 2023052600
 );
