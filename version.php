8422a6ae7124f99f9de19b29b354205e983e7278
Matthias Kollenbroich <matthias.kollenbroich@uni-muenster.de>
2023-08-03 10:51:03 +0200

    Created branch Release_v4.2 and updated version.php files

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_opencast';
-$plugin->release   = 'development-version';
-$plugin->version = 2023030100;
+$plugin->release   = 'v4.2-r1';
+$plugin->version = 2023080100;
 $plugin->requires = 2020061500; // Requires Moodle 3.9+.
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'tool_opencast' => 2023030100,
-    'mod_opencast' => 2023030100
+    'tool_opencast' => 2023080100,
+    'mod_opencast' => 2023080100
 );
