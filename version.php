a7aa8507c21f235e72fee52fafb4aff2c3bb54b3
Thomas Niedermaier <thomas.niedermaier@uni-muenster.de>
2026-07-22 16:23:31 +0200

    version.php, changelog

--- version.php
+++ version.php
@@ -25,12 +25,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_opencast';
-$plugin->release = 'v5.1-r1';
-$plugin->version = 2026011700;
+$plugin->release = 'v5.1-r2';
+$plugin->version = 2026011701;
 $plugin->requires = 2025041400; // Requires Moodle 5.0+.
 $plugin->supported = [500, 501];
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'tool_opencast' => 2025080100,
-    'mod_opencast' => 2025080100,
+    'tool_opencast' => 2026011701,
+    'mod_opencast' => 2026011702,
 ];
